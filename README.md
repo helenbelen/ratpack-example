09124b4e2ae52611b49bad252a82619ed31b4f5f
HelenAgha <helen.agha@target.com>
2019-03-27 15:45:04 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![forthebadge](https://forthebadge.com/images/badges/designed-in-ms-paint.svg)](https://forthebadge.com)
+
 # ratpack-example
-exploring ratpack 
+exploring ratpack
---
+++ ratpack.groovy
@@ -0,0 +1,18 @@
+@Grapes([
+  @Grab('io.ratpack:ratpack-groovy:1.6.0'),
+  @Grab('org.slf4j:slf4j-simple:1.7.25')
+  ])
+
+  import static ratpack.groovy.Groovy.ratpack
+
+  ratpack {
+    handlers {
+      get {
+        render "Hello World!"
+      }
+      get(":name"){
+        render "Hello $pathToken.name!"
+      }
+    }
+
+  }
